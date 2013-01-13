10342717a1c3e7b2ac21a22f24a2323c647be91e
Alexander Ose <alex@alexose.com>
2013-01-13 14:31:11 -0800

    remove unnecessary fields

--- application/controllers/auth.php
+++ application/controllers/auth.php
@@ -200,8 +200,6 @@ class Auth extends MY_Controller {
             {
                 $this->data['identity_label'] = 'Email';    
             }
-            
-            
 
             //set any errors and display the form
             $this->data['message'] = (validation_errors()) ? validation_errors() : $this->session->flashdata('message');
@@ -393,27 +391,17 @@ class Auth extends MY_Controller {
         $this->data['title'] = "Create User";
 
         //validate form input
-        $this->form_validation->set_rules('first_name', 'First Name', 'required|xss_clean');
-        $this->form_validation->set_rules('last_name', 'Last Name', 'required|xss_clean');
         $this->form_validation->set_rules('email', 'Email Address', 'required|valid_email');
-        $this->form_validation->set_rules('phone1', 'First Part of Phone', 'required|xss_clean|min_length[3]|max_length[3]');
-        $this->form_validation->set_rules('phone2', 'Second Part of Phone', 'required|xss_clean|min_length[3]|max_length[3]');
-        $this->form_validation->set_rules('phone3', 'Third Part of Phone', 'required|xss_clean|min_length[4]|max_length[4]');
-        $this->form_validation->set_rules('company', 'Company Name', 'required|xss_clean');
         $this->form_validation->set_rules('password', 'Password', 'required|min_length[' . $this->config->item('min_password_length', 'ion_auth') . ']|max_length[' . $this->config->item('max_password_length', 'ion_auth') . ']|matches[password_confirm]');
         $this->form_validation->set_rules('password_confirm', 'Password Confirmation', 'required');
 
         if ($this->form_validation->run() == true)
         {
-            $username = strtolower($this->input->post('first_name')) . ' ' . strtolower($this->input->post('last_name'));
+            $username = $this->input->post('email');
             $email    = $this->input->post('email');
             $password = $this->input->post('password');
 
             $additional_data = array(
-                'first_name' => $this->input->post('first_name'),
-                'last_name'  => $this->input->post('last_name'),
-                'company'    => $this->input->post('company'),
-                'phone'      => $this->input->post('phone1') . '-' . $this->input->post('phone2') . '-' . $this->input->post('phone3'),
             );
         }
         if ($this->form_validation->run() == true && $this->ion_auth->register($username, $password, $email, $additional_data))
@@ -434,47 +422,11 @@ class Auth extends MY_Controller {
             //set the flash data error message if there is one
             $this->data['message'] = (validation_errors() ? validation_errors() : ($this->ion_auth->errors() ? $this->ion_auth->errors() : $this->session->flashdata('message')));
 
-            $this->data['first_name'] = array(
-                'name'  => 'first_name',
-                'id'    => 'first_name',
-                'type'  => 'text',
-                'value' => $this->form_validation->set_value('first_name'),
-            );
-            $this->data['last_name'] = array(
-                'name'  => 'last_name',
-                'id'    => 'last_name',
-                'type'  => 'text',
-                'value' => $this->form_validation->set_value('last_name'),
-            );
             $this->data['email'] = array(
                 'name'  => 'email',
                 'id'    => 'email',
                 'type'  => 'text',
                 'value' => $this->form_validation->set_value('email'),
-            );
-            $this->data['company'] = array(
-                'name'  => 'company',
-                'id'    => 'company',
-                'type'  => 'text',
-                'value' => $this->form_validation->set_value('company'),
-            );
-            $this->data['phone1'] = array(
-                'name'  => 'phone1',
-                'id'    => 'phone1',
-                'type'  => 'text',
-                'value' => $this->form_validation->set_value('phone1'),
-            );
-            $this->data['phone2'] = array(
-                'name'  => 'phone2',
-                'id'    => 'phone2',
-                'type'  => 'text',
-                'value' => $this->form_validation->set_value('phone2'),
-            );
-            $this->data['phone3'] = array(
-                'name'  => 'phone3',
-                'id'    => 'phone3',
-                'type'  => 'text',
-                'value' => $this->form_validation->set_value('phone3'),
             );
             $this->data['password'] = array(
                 'name'  => 'password',
@@ -514,19 +466,7 @@ class Auth extends MY_Controller {
         $groups=$this->ion_auth->groups()->result_array();
         $currentGroups = $this->ion_auth->get_users_groups($id)->result();
         
-        //process the phone number
-        if (isset($user->phone) && !empty($user->phone))
-        {
-            $user->phone = explode('-', $user->phone);
-        }
-
         //validate form input
-        $this->form_validation->set_rules('first_name', 'First Name', 'required|xss_clean');
-        $this->form_validation->set_rules('last_name', 'Last Name', 'required|xss_clean');
-        $this->form_validation->set_rules('phone1', 'First Part of Phone', 'required|xss_clean|min_length[3]|max_length[3]');
-        $this->form_validation->set_rules('phone2', 'Second Part of Phone', 'required|xss_clean|min_length[3]|max_length[3]');
-        $this->form_validation->set_rules('phone3', 'Third Part of Phone', 'required|xss_clean|min_length[4]|max_length[4]');
-        $this->form_validation->set_rules('company', 'Company Name', 'required|xss_clean');
         $this->form_validation->set_rules('groups', 'Groups', 'xss_clean');
         
         if (isset($_POST) && !empty($_POST))
@@ -538,10 +478,6 @@ class Auth extends MY_Controller {
             }
 
             $data = array(
-                'first_name' => $this->input->post('first_name'),
-                'last_name'  => $this->input->post('last_name'),
-                'company'    => $this->input->post('company'),
-                'phone'      => $this->input->post('phone1') . '-' . $this->input->post('phone2') . '-' . $this->input->post('phone3'),
             );
             
             //Update the groups user belongs to
@@ -588,42 +524,6 @@ class Auth extends MY_Controller {
         $this->data['groups'] = $groups;
         $this->data['currentGroups'] = $currentGroups;
 
-        $this->data['first_name'] = array(
-            'name'  => 'first_name',
-            'id'    => 'first_name',
-            'type'  => 'text',
-            'value' => $this->form_validation->set_value('first_name', $user->first_name),
-        );
-        $this->data['last_name'] = array(
-            'name'  => 'last_name',
-            'id'    => 'last_name',
-            'type'  => 'text',
-            'value' => $this->form_validation->set_value('last_name', $user->last_name),
-        );
-        $this->data['company'] = array(
-            'name'  => 'company',
-            'id'    => 'company',
-            'type'  => 'text',
-            'value' => $this->form_validation->set_value('company', $user->company),
-        );
-        $this->data['phone1'] = array(
-            'name'  => 'phone1',
-            'id'    => 'phone1',
-            'type'  => 'text',
-            'value' => $this->form_validation->set_value('phone1', $user->phone[0]),
-        );
-        $this->data['phone2'] = array(
-            'name'  => 'phone2',
-            'id'    => 'phone2',
-            'type'  => 'text',
-            'value' => $this->form_validation->set_value('phone2', $user->phone[1]),
-        );
-        $this->data['phone3'] = array(
-            'name'  => 'phone3',
-            'id'    => 'phone3',
-            'type'  => 'text',
-            'value' => $this->form_validation->set_value('phone3', $user->phone[2]),
-        );
         $this->data['password'] = array(
             'name' => 'password',
             'id'   => 'password',
--- application/views/auth/edit_user.php
+++ application/views/auth/edit_user.php
@@ -4,27 +4,6 @@
 <div id="infoMessage"><?php echo $message;?></div>
 
 <?php echo form_open(current_url());?>
-
-      <p>
-            First Name: <br />
-            <?php echo form_input($first_name);?>
-      </p>
-
-      <p>
-            Last Name: <br />
-            <?php echo form_input($last_name);?>
-      </p>
-
-      <p>
-            Company Name: <br />
-            <?php echo form_input($company);?>
-      </p>
-
-      <p>
-            Phone: <br />
-            <?php echo form_input($phone1);?>-<?php echo form_input($phone2);?>-<?php echo form_input($phone3);?>
-      </p>
-
       <p>
             Password: (if changing password)<br />
             <?php echo form_input($password);?>
