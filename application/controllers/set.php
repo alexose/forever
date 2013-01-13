8e91c591b75457e78bb10b98a340573d3a1222e2
Alexander Ose <alex@alexose.com>
2013-01-12 18:08:11 -0800

    add set scaffold

---
+++ application/controllers/set.php
@@ -0,0 +1,31 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Home extends MY_Controller {
+
+    function __construct()
+    {  
+        parent::__construct();
+        if (!$this->ion_auth->logged_in()) redirect('auth/login', 'refresh');
+        $this->load->view('partials/header', $this->headerViewData());
+    }  
+	
+	public function index()
+	{
+		$this->load->view('home');
+		$this->load->view('partials/footer');
+	}
+	
+    public function add()
+	{
+		$this->load->view('home');
+		$this->load->view('partials/footer');
+	}
+	
+    public function remove()
+	{
+		$this->load->view('home');
+		$this->load->view('partials/footer');
+	}
+	
+}
+
